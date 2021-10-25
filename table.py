95f1a5674a1ad97e81c73f16d4889b147ea49df3
Waleed Malik <31138706+waleedmalik97@users.noreply.github.com>
2021-10-25 20:01:36 +0500

    Update table.py

--- table.py
+++ table.py
@@ -8,7 +8,7 @@ import plotly.graph_objects as go
 pd.options.mode.chained_assignment = None
 
 
-res = requests.get('https://data.tradeui.com/tui_sweeps?idT=aa')
+res = requests.get('Your API Link')
 
 res = res.json() # convert to json
 res = np.array(res)
